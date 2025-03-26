81afff03f63d4a3f33e4ed028298410aaf6e5ee8
ringinrp <ringinrp21@gmail.com>
2025-03-26 14:14:55 +0700

    update: button navbar, dropdown, navigate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../assets/assets_frontend/assets'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const [showMenu, setShowMenu] = useState(false);
+
+    const[token, setShowToken] = useState(true);
+    
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
         <img className='w-44 cursor-pointer' src={assets.logo} alt='navbar'/>
@@ -25,7 +32,20 @@ const Navbar = () => {
             </NavLink>
         </ul>
         <div className='flex gap-5 items-center'>
-            <button className='bg-[#5f6FFF] text-white rounded-full px-8 py-3 font-light hidden md:block'> Create account</button>
+            {
+                token ? <div className='flex items-center gap-2 cursor-pointer group relative'>
+                    <img className='w-8 rounded-full' src={assets.profile_pic} alt=''/>
+                    <img className='w-2.5' src={assets.dropdown_icon} alt=''/>
+                    <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+                        <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                            <p onClick={()=>navigate('my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
+                            <p onClick={()=>navigate('my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
+                            <p className='hover:text-black cursor-pointer'>Logout</p>
+                        </div>
+                    </div>
+                </div> : <button onClick={()=>navigate('/login')} className='bg-[#5f6FFF] text-white rounded-full px-8 py-3 font-light hidden cursor-pointer md:block'> Create account</button>
+            }
+            
         </div>
     </div>
   )
