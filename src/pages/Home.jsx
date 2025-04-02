6665099f669078924d9c205ec51c38edaf2d12e0
ringinrp <ringinrp21@gmail.com>
2025-04-03 06:28:07 +0700

    update: add Banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import SpecialistMenu from '../components/SpecialistMenu'
 import TopDoctor from '../components/TopDoctor'
+import Banner from './Banner'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Header />
         <SpecialistMenu />
         <TopDoctor />
+        <Banner />
     </div>
   )
 }
