43a4215b03d9950289854b66eced4d9d03e3e687
ringinrp <ringinrp21@gmail.com>
2025-04-04 10:11:10 +0700

    fix: speciality to specialist

--- src/components/SpecialistMenu.jsx
+++ src/components/SpecialistMenu.jsx
@@ -9,9 +9,9 @@ const SpecialistMenu = () => {
         <p className="sm:w-1/3 text-sm text-center">Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free.</p>
         <div className="flex sm:justify-center gap-4 pt-5 w-full overflow-scroll">
             {specialityData.map((item, index) => (
-                <Link onClick={()=>scrollTo(0,0)} className="flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-300" to={`/doctors/${item.speciality}`} key={index} >
+                <Link onClick={()=>scrollTo(0,0)} className="flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-300" to={`/doctors/${item.specialist}`} key={index} >
                     <img className="w-16 sm:w-24 mb-2" src={item.image} alt={item.name} />
-                    <p>{item.speciality}</p>
+                    <p>{item.specialist}</p>
                 </Link>
             ))}
         </div>
