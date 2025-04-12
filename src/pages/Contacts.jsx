8a6e24e49375b151f87a2fb1a9130da8d9661bb5
ringinrp <ringinrp21@gmail.com>
2025-04-12 09:37:50 +0700

    update: add contact

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { assets } from '../assets/assets_frontend/assets'
 
 const Contacts = () => {
   return (
     <div>
-      Contact
+        <div className='text-center text-2xl pt-10 text-gray-500'>
+            <p>Contact <span className='text-gray-700 font-medium'>US</span></p>
+        </div>
+
+        <div className='flex flex-col my-10 justify-center items-center md:flex-row gap-10 bm-28 text-sm'>
+            <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt=''/>
+            <div className='flex flex-col justify-center items-start gap-6'>
+              <b className="font-semibold text-lg text-gray-600">OUR OFFICE</b>
+              <p className='text-gray-500'>00000 Willms Station <br/>Suite 000, Washington, USA</p>
+              <p className='text-gray-500'>Tel: (000) 000-0000 <br/>Email: greatstackdev@gmail.com</p>
+              <b className="font-semibold text-lg text-gray-600">CAREERS AT PRESCRIPTO</b>
+              <p className='text-gray-500'>Learn more about our teams and job openings.</p>
+            <button className='border border-black px-8 py-4 text-sm hover:bg-[#5f6FFF] hover:border-none hover:text-white transition-all duration-500 cursor-pointer'>Explore Jobs</button>
+            </div>
+        </div>
     </div>
   )
 }
