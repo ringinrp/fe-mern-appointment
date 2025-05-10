07ea07df9a962c84f194313068e926aba40c65cb
ringinrp <ringinrp21@gmail.com>
2025-05-10 20:48:14 +0700

    update: navbar mobile, doctor

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -25,7 +25,7 @@ const Doctors = () => {
   return (
     <div>
       <p className='text-gray-600'>Browse through the doctors specialist.</p>
-      <div className=' flex flex-col sm:flex-row items-start gap-4 mt-5'>
+      <div className=' flex flex-col sm:flex-row items-start gap-5 mt-5'>
         <div className='flex flex-col gap-4 text-sm text-gray-600'>
             <p onClick={() => specialist === 'General physician' ? navigate('/doctors') : navigate('/doctors/General physician')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${specialist === "General physician" ? "bg-indigo-100 text-black" : ""}`}>General physician</p>
             <p onClick={() => specialist === 'Gynecologist' ? navigate('/doctors') : navigate('/doctors/Gynecologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${specialist === "Gynecologist" ? "bg-indigo-100 text-black" : ""}`}>Gynecologist</p>
